facbf1c06d66b870d3e538229b657a85eeddf004
Chen <1102361302@qq.com>
2023-08-03 18:28:33 +0800

    add license

--- full/src/main/java/com/whaleal/ddt/task/generate/Range.java
+++ full/src/main/java/com/whaleal/ddt/task/generate/Range.java
@@ -1,3 +1,18 @@
+/*
+ * MongoT - An open-source project licensed under GPL+SSPL
+ *
+ * Copyright (C) [2023 - present ] [Whaleal]
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License and Server Side Public License (SSPL) as published by
+ * the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License and SSPL for more details.
+ *
+ * For more information, visit the official website: [www.whaleal.com]
+ */
 package com.whaleal.ddt.task.generate;
 
 
