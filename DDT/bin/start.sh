52282428d7b58a4f6916f0c4fed4ceef2cb073a6
GitHubLhp123 <2446268838@qq.com>
2023-08-09 17:09:57 +0800

    修改配置文件

--- DDT/bin/start.sh
+++ DDT/bin/start.sh
@@ -1,19 +1,26 @@
-echo "                              _ _   _____
-  _ __  ___ _ _  __ _ ___  __| | |_|_   _|
- | '  \/ _ \ ' \/ _' / _ \/ _' | '_ \| |
- |_|_|_\___/_||_\__, \___/\__,_|_.__/|_|
-                |___/                       "
+echo "  ____    ____    _____ "
+echo " |  _ \\  |___ \\  |_   _|"
+echo " | | | |   __) |   | |  "
+echo " | |_| |  / __/    | |  "
+echo " |____/  |_____|   |_|  "
+echo "                        "
+
 echo "启动"
-# 若没有mongodbT.pid文件 则创建
-if [ ! -f "mongodbT.pid" ]; then
-  touch "mongodbT.pid"
+# 若没有 DDT.pid 文件则创建
+if [ ! -f "DDT.pid" ]; then
+  touch "DDT.pid"
 fi
-# 检查是否有实例启动pid
-pid=$(cat mongodbT.pid)
+# 检查是否有实例启动 pid
+pid=$(cat DDT.pid)
 if [ ${#pid} -gt 0 ]; then
-  echo "有其他已经运行的MongodbT任务请检查"
+  echo "有其他已经运行的 DDT 任务请检查"
   exit
 fi
 
-nohup java -jar ../execute-1.0-SNAPSHOT.jar ../config/mongodbT.properties >/dev/null 2>&1 &
-echo $! >mongodbT.pid
+# 获取主机内存大小（以MB为单位）
+total_memory=$(free -m | grep 'Mem:' | awk '{print $2}')
+# 计算最大堆内存大小为主机内存的80%
+max_heap_size=$((total_memory * 80 / 100))M
+
+nohup java -Xmx$max_heap_size -jar ../execute-1.0-SNAPSHOT.jar ../config/DDT.properties >/dev/null 2>&1 &
+echo $! >DDT.pid
--- DDT/bin/stop.sh
+++ DDT/bin/stop.sh
@@ -1,13 +1,14 @@
-if [ ! -f "mongodbT.pid" ]; then
+if [ ! -f "DDT.pid" ]; then
   echo "没有启动任何实例程序"
   exit
 fi
-# 检查是否有实例启动pid
-pid=$(cat mongodbT.pid)
+
+# 检查是否有实例启动 pid
+pid=$(cat DDT.pid)
 if [ ${#pid} -gt 0 ]; then
   kill -9 ${pid}
   echo "${pid}程序已关闭"
-  echo "" > mongodbT.pid
+  echo "" > DDT.pid
 else
   echo "无运行中的程序关闭"
 fi
